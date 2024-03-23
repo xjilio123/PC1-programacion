0af353f2cc02bd2a540e94bddfab71e615e27974
xjilio123 <joaquin200314@gmail.com>
2024-03-23 09:47:22 -0500

    Ultimo Commit - jcr

--- TechStore/obj/Debug/net8.0/TechStore.AssemblyInfo.cs
+++ TechStore/obj/Debug/net8.0/TechStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb64bf25b68d440a4408572d67358235d7abb5c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
